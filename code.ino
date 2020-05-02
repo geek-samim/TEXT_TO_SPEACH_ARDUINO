80c2cca90b14e87cf569d7f38dcdd4c8e0157557
Samim Islam <samimislam9641@gmail.com>
2020-05-02 22:52:23 +0530

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,17 @@
+#include "Talkie.h"
+#include "Vocab_US_Large.h"
+#include "Vocab_Special.h"
+
+Talkie voice;
+
+void setup() {
+}
+void loop() {
+    voice.say(spPAUSE2);
+    voice.say(sp2_DANGER);
+    voice.say(sp2_DANGER);
+    voice.say(sp3_STORM);
+    voice.say(sp3_IN);
+    voice.say(sp3_THE);
+    voice.say(sp3_NORTH);
+}
